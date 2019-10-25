e1c4af098b6a95f38a57983790690829ce21cfa3
vtorres96 <victorcdc96@gmail.com>
2019-10-25 11:22:26 -0300

    adding functions

---
+++ utils/salvar-usuario.php
@@ -0,0 +1,32 @@
+<?php
+
+    // incluir o arquivo de conexão com banco de dados
+    require_once("../config/conn.php");
+
+    // incluir o arquivo que retorna a mensagem no front-end sem refresh
+    require_once("./return-message.php");
+
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    if(!strlen($nome) || !strlen($email) || !strlen($senha)){
+        echo retornaMensagem("Todos os campos devem ser preenchidos");
+        return;
+    }
+
+    $query = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)");
+
+    $salvou = $query->execute([
+        ":nome" => $nome, 
+        ":email" => $email,
+        ":senha" => password_hash($senha, PASSWORD_DEFAULT)
+    ]);
+    
+    if(isset($salvou) && $salvou == true){
+        echo retornaMensagem("Usuário cadastrado com sucesso", true);
+    } else {
+        echo retornaMensagem("Falha ao processar requisição");
+    }
+
+?>
