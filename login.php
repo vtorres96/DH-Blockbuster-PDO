b581367f9cd2bd5e17f0df8bbf314d164a918b7b
vtorres96 <victorcdc96@gmail.com>
2019-12-29 16:42:19 -0300

    updated session login and action form

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
     <main class="container pt-5 pb-5">
         <h1>Login</h1>
         <div class="col-lg-6">
-            <form id="formulario" action="utils/valida-login.php" method="POST">
+            <form id="formulario" action="utils/auth/valida-login.php" method="POST">
                 <div class="form-group">
                     <label for="exampleInputEmail">E-mail</label>
                     <input type="email" name="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Entre com e-mail">
