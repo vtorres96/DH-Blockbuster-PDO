4e72e2f84f745c41366020d499e2646880575dd8
vtorres96 <victorcdc96@gmail.com>
2019-12-29 16:41:40 -0300

    updated navbar to logged user

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,47 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand mr-auto" href="index.php">Blockbuster</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav ml-auto">    
-        <a class="nav-item nav-link text-white" href="#">Home</a>
-        <a class="nav-item nav-link text-white" href="#filmes">Filmes</a>
-        <a class="nav-item nav-link text-white" href="#localizacao">Localização</a>
-        <a class="nav-item nav-link text-white" href="cadastro.php">Cadastre-se</a>
-        <a class="nav-item nav-link text-white" href="login.php">Login</a>
+<?php
+  if(!isset($_SESSION)) { 
+      session_start(); 
+  } 
+  if($_SESSION){
+    $logado = $_SESSION["logado"];
+    $nome = $_SESSION["nome"];
+    
+    if($logado){
+      $active = "admin";
+    }
+  } else { 
+    $active = "comum";
+  }
+?>
+
+<?php if($active == "comum"): ?>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand mr-auto" href="index.php">Blockbuster</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto">    
+          <a class="nav-item nav-link text-white" href="#">Home</a>
+          <a class="nav-item nav-link text-white" href="#filmes">Filmes</a>
+          <a class="nav-item nav-link text-white" href="#localizacao">Localização</a>
+          <a class="nav-item nav-link text-white" href="cadastro.php">Cadastre-se</a>
+          <a class="nav-item nav-link text-white" href="login.php">Login</a>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+<?php elseif($active == "admin"): ?>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand mr-auto" href="index.php">Blockbuster</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto">    
+          <a class="nav-item nav-link text-white" href="#">Home</a>
+          <a class="nav-item nav-link text-white" href="#">Olá, <?= $nome ?></a>
+          <a class="nav-item nav-link text-white" href="utils/auth/logout.php">Sair</a>
+      </div>
+    </div>
+  </nav>
+<?php endif; ?>
