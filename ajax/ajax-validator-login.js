b3367a15b7f3d0b3e156a3287ba14ace536c1c3a
vtorres96 <victorcdc96@gmail.com>
2019-12-31 16:09:53 -0300

    updated ajax

--- ajax/ajax-validator-login.js
+++ ajax/ajax-validator-login.js
@@ -10,7 +10,7 @@ $(function ($) {
           if (retorno.sucesso) {
             mensagem.attr('class', 'alert alert-success mt-2')
             formulario.resetForm()
-            $(location).attr('href', 'index.php');
+            $(location).attr('href', '../../views/default/index.php');
           } else {
             mensagem.attr('class', 'alert alert-danger mt-2')
           }
